975e6cec489e59074ca68951ffbe1af19000d681
硯川 千優 Suzurikawa <64934951+chihiros@users.noreply.github.com>
2022-11-25 10:29:23 +0900

    GTM追加

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,6 +49,10 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+	gtag: {
+	  trackingID: 'G-EPXC0BNY78',
+	  anonymizeIP: true,
+	},
       }),
     ],
   ],
